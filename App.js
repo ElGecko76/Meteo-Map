a792f8b4271fe2ba109b0bf89f7640570e80790a
Valentin <contact@valentin-harrang.fr>
2020-04-05 23:04:14 +0200

    Ajout Redux

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import SearchScreen from './screens/SearchScreen';
+import store from "./store";
+import { Provider } from "react-redux";
 
 export default class App extends Component {
     render() {
         return (
-            <SearchScreen />
+            <Provider store={store}>
+                <SearchScreen />
+            </Provider>
         );
     }
 }
