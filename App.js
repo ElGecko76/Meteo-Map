41d8767bb67f8ea603fe2af17ed52420da86a6a7
Valentin <contact@valentin-harrang.fr>
2020-04-05 20:00:51 +0200

    Ajout Google Maps

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+const DEFAULT_COORD = { lat: 48.859268, lng: 2.347060 }
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                region={{ latitude: DEFAULT_COORD.lat, longitude: DEFAULT_COORD.lng, latitudeDelta: 0.2000, longitudeDelta: 0.1000 }}
+                scrollEnabled={false}
+                liteMode={true}>
+            </MapView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1
+    },
+    map: {
+        flex: 1
+    }
 });
