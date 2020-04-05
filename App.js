8e019530d2600b45972ca0e728407493d7eec0b2
Valentin <contact@valentin-harrang.fr>
2020-04-05 20:31:12 +0200

    Ajout barre de recherche

--- App.js
+++ App.js
@@ -1,27 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { Component } from 'react';
+import SearchScreen from './screens/SearchScreen';
 
-const DEFAULT_COORD = { lat: 48.859268, lng: 2.347060 }
-
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <MapView
-                style={styles.map}
-                region={{ latitude: DEFAULT_COORD.lat, longitude: DEFAULT_COORD.lng, latitudeDelta: 0.2000, longitudeDelta: 0.1000 }}
-                scrollEnabled={false}
-                liteMode={true}>
-            </MapView>
-        </View>
-    );
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    map: {
-        flex: 1
+export default class App extends Component {
+    render() {
+        return (
+            <SearchScreen />
+        );
     }
-});
+}
---
+++ screens/SearchScreen.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { SearchBar } from 'react-native-elements';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
+
+const DEFAULT_COORD = { lat: 48.859268, lng: 2.347060 }
+
+export default class SearchScreen extends Component {
+    state = { search: "" }
+
+    updateSearch = search => {
+        this.setState({ search })
+    }
+
+    submitSearch = () => {
+        console.log(this.state.search);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    region={{ latitude: DEFAULT_COORD.lat, longitude: DEFAULT_COORD.lng, latitudeDelta: 0.2000, longitudeDelta: 0.1000 }}
+                    scrollEnabled={false}
+                    liteMode={true} />
+
+                <SearchBar
+                    lightTheme
+                    onChangeText={this.updateSearch}
+                    value={this.state.search}
+                    onSubmitEditing={this.submitSearch}
+                    placeholder="Type your city..."
+                    containerStyle={{
+                        position: "absolute",
+                        bottom: hp("50%"),
+                        left: wp("5%"),
+                        width: wp("90%")
+                    }} />
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    map: {
+        flex: 1
+    }
+});
